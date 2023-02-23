3426b7eca6393ec7ab975bd84f38759217ab5ac6
Glow05 <glowngoya@gmail.com>
2023-02-23 08:44:50 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isuper - Check if a letter is uppercase
+ * @x: The number to be checked
+ * Return: 1 for upper letter otherwise 0
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
